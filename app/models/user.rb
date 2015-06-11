a43e184426aab2250090246bdd683228ccb62f05
Buddhi DeSilva <buddhi.desilva@gmail.com>
2015-06-12 03:58:56 +0530

    * Production Env:  Make only warnings shows up to avoid leaking of devise keys etc. * Devise mailing method: Send through queue instead of direct sending

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  # :registerable # being removed for the time being.
   devise :database_authenticatable, :recoverable,
          :rememberable, :trackable, :validatable
-  # :registerable # being removed for the time being.
+
+  # Use active job to send mails through a queu instead of direct sending
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
 end
