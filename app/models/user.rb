8f2b3ce0dafb86698b5a00a22600855c63526ad4
Buddhi DeSilva <buddhi.desilva@gmail.com>
2015-06-12 03:46:51 +0530

    registrable removed until caffa allows user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
+# Devise user model
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable,
+         :rememberable, :trackable, :validatable
+  # :registerable # being removed for the time being.
 end
