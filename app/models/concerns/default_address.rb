7ca94198373095d0cd1e87045419ad4b028fb70d
Buddhi DeSilva <buddhi.desilva@gmail.com>
2015-07-27 13:43:58 +0530

    get validation errors if the address is defined

--- app/models/concerns/default_address.rb
+++ app/models/concerns/default_address.rb
@@ -6,10 +6,11 @@ module DefaultAddress
     has_one :address, as: :addressable, dependent: :destroy
     accepts_nested_attributes_for :address
 
-    # validates_associated :address
     validate do |record|
-      address.errors.full_messages.each do |msg|
-        errors[:base] << msg
+      if address
+        address.errors.full_messages.each do |msg|
+          errors[:base] << msg
+        end
       end
     end
 
