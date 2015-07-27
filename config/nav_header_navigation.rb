2f303ea62f9cd7999ba598de712ff7094cda8b52
Buddhi DeSilva <buddhi.desilva@gmail.com>
2015-07-27 13:57:15 +0530

    update caffa 2015 links

--- config/nav_header_navigation.rb
+++ config/nav_header_navigation.rb
@@ -5,9 +5,12 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.item :coffee_event, "#{image_tag 'img-coffee-event.png', alt: t('nav.header.coffee_event.coffee_event')}".html_safe + "#{t('nav.header.coffee_event.coffee_event')}", '#' do |coffee_event|
       coffee_event.dom_id = 'coffee_event_sub'
       coffee_event.dom_class = 'sub'
-      coffee_event.item :program, t('nav.header.coffee_event.program'), page_url(id: 'coffee-event-program')
-      coffee_event.item :sponsors, t('nav.header.coffee_event.sponsors'), page_url(id: 'coffee-event-sponsors')
-      coffee_event.item :photo_log, t('nav.header.coffee_event.photo_log'), page_url(id: 'coffee-event-photo-log')
+      coffee_event.item :program, t('nav.header.coffee_event.program'), page_url(id: 'program')
+      coffee_event.item :sponsors, t('nav.header.coffee_event.sponsors'), page_url(id: 'sponsors')
+      coffee_event.item :photo_log, t('nav.header.coffee_event.location_and_floorplan'), page_url(id: 'location-and-floorplan')
+      coffee_event.item :photo_log, t('nav.header.coffee_event.visitors_info'), page_url(id: 'visitors-info')
+      coffee_event.item :photo_log, t('nav.header.coffee_event.vendors_info'), page_url(id: 'vendors-info')
+      coffee_event.item :photo_log, t('nav.header.coffee_event.photo_log'), page_url(id: 'photo-log')
     end
     primary.item :coffee_adventure, "#{image_tag 'img-coffee-adventure.png', alt: t('nav.header.coffee_adventure.coffee_adventure')}".html_safe + "#{t('nav.header.coffee_adventure.coffee_adventure')}", '#' do |coffee_adventure|
       coffee_adventure.dom_id = 'coffee_adventure_sub'
